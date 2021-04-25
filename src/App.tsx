11670d1b4da0c2586f9c6735d01ab82e5a81fc1a
Chia Pak Mun <pakmunchia@ifca.com.my>
2021-04-25 13:50:04 +0800

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> update version notification </p>
+        <p> PWA Update Version notification </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          😂😂😁😁😁
         </a>
       </header>
     </div>
