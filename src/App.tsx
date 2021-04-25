a51120ab08d13c49d3aa098d12d2edc250744106
Chia Pak Mun <pakmunchia@ifca.com.my>
2021-04-25 13:36:18 +0800

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>PWA update version notification snackbar!!</p>
+        <p>PWA update version notification snackbar</p>
         <a
           className="App-link"
           href="https://reactjs.org"
